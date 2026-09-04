4371f03ff1d73a1140f8cc4e730ba1bedc008a4a
unknown <rintaro.chiba@helloworld.works>
2026-09-04 15:45:31 +0900

    演算子前後に空白を挿入

--- server/src/Kadai1/Kadai5_2.java
+++ server/src/Kadai1/Kadai5_2.java
@@ -8,11 +8,11 @@ package Kadai1;
 public class Kadai5_2 {
     public static void FizzBuzz(){
         for (int i = 1; i <= 30; i++){
-            if (i%3==0 && i%5==0){
+            if (i % 3 == 0 && i % 5 == 0){
                 System.out.println("FizzBuzz");
-            } else if (i%3==0){
+            } else if (i % 3 == 0){
                 System.out.println("Fizz");
-            } else if (i%5==0){
+            } else if (i % 5 == 0){
                 System.out.println("Buzz");
             }else{
                 System.out.println(i);
